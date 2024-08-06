cfd765611350eaa905bc2ff392a2de3a5ba118e2
bubu-destroi <zzzarolina@gmail.com>
2024-08-06 21:32:06 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src='https://cdn.myportfolio.com/60cb4387-4320-4a64-9df1-40a0d496f12d/bc6a13c3-fbb5-4eeb-b0aa-f0cb987bb1f6.png?h=5acb79b7d88727c455c86674a58df7a0' className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src='https://cdn.myportfolio.com/60cb4387-4320-4a64-9df1-40a0d496f12d/bc6a13c3-fbb5-4eeb-b0aa-f0cb987bb1f6.png?h=5acb79b7d88727c455c86674a58df7a0' className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>OXITOFICINA</h1>
+      <div className="card">
+        <p>
+          what do you want to learn?
+        </p>
+      </div>
+      <p className="read-the-docs">
+       
+      </p>
+    </>
+  )
+}
+
+export default App

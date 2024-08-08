2149bf710e705ae30e71fc9853f4e518f6c79070
bubu-destroi <zzzarolina@gmail.com>
2024-08-08 21:06:39 +0100

    login page, authContext, .env

---
+++ context/auth.context.jsx
@@ -0,0 +1,54 @@
+import { useState, useEffect, createContext } from 'react';
+import axios from 'axios';
+
+const AuthContext = createContext();
+
+function AuthProviderWrapper(props) {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+
+  const storeToken = (token) => {
+    localStorage.setItem('authToken', token);
+  };
+  const authenticateUser = async () => {
+    const storedToken = localStorage.getItem('authToken');
+
+    if (storedToken) {
+      try {
+        setLoading(true);
+        const response = await axios.get(
+          `${import.meta.env.VITE_API_URL}/auth/verify`,
+          {
+            headers: {
+              Authorization: `Bearer ${storedToken}`,
+            },
+          }
+        );
+        setUser(response.data);
+        setLoading(false);
+      } catch (error) {
+        setUser(null);
+        setLoading(false);
+      }
+    } else {
+      setUser(null);
+      setLoading(false);
+    }
+  };
+  const logout = () => {
+    localStorage.removeItem('authToken');
+    authenticateUser();
+  };
+  useEffect(() => {
+    authenticateUser();
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{ loading, user, storeToken, authenticateUser, logout }}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+}
+
+export { AuthContext, AuthProviderWrapper };
---
+++ pages/Auth/Login.jsx
@@ -0,0 +1,61 @@
+import { useState, useContext } from 'react';
+import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../context/auth.context';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState(null);
+  const { storeToken, authenticateUser } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  const handleEmail = (e) => setEmail(e.target.value);
+  const handlePassword = (e) => setPassword(e.target.value);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_API_URL}/auth/login`,
+        { email, password }
+      );
+      storeToken(response.data.authToken);
+      console.log(response.data.authToken);
+      authenticateUser();
+      navigate('/');
+    } catch (error) {
+      setErrorMessage(error.response.data.message);
+    }
+  };
+  return (
+    <div className='loginPage'>
+      <h2>please log in to start learning!</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='email'>your email</label>
+        <input
+          type='email'
+          name='email'
+          id='email'
+          value={email}
+          onChange={handleEmail}
+        />
+        <label htmlFor='password'>your password</label>
+        <input
+          type='password'
+          name='password'
+          id='password'
+          value={password}
+          onChange={handlePassword}
+        />
+        <button type='submit'>lets go!</button>
+      </form>
+      {errorMessage && <p>{errorMessage}</p> }
+      <p>don't have an account?</p>
+      <Link to='/signup'>create your account here :> </Link>
+    </div>
+  );
+}
+
+export default Login

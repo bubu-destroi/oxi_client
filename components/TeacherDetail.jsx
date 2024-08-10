691c4f7fd870c5ea8fc6c94e8260ce27c827b4b7
bubu-destroi <zzzarolina@gmail.com>
2024-08-10 23:03:35 +0100

    finished TeacherDetail

--- components/TeacherDetail.jsx
+++ components/TeacherDetail.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function TeacherDetail() {
+
   const [teacher, setTeacher] = useState(null);
   //const navigate = useNavigate();
   const { teacherID } = useParams();
@@ -14,25 +15,30 @@ function TeacherDetail() {
         `${import.meta.env.VITE_API_URL}/api/teachers/${id}`
       );
       setTeacher(response.data);
-      console.log(response);
-      console.log(teacher);
+      console.log(response.data);
+     // console.log(teacher);
     } catch (error) {
       console.log('error', error);
     }
   };
+
   useEffect(() => {
     getSingleTeacher(teacherID);
-  }, []);
+  }, [teacherID]);
 
   return (
     <>
-      <div>a closer look</div>
+      <div>a closer look at</div>
       {teacher && (
-        <div>
+        <div key={teacher._id}>
           <h3>{teacher.name}</h3>
           <h5>{teacher.bio}</h5>
           <h5>{teacher.socialMedia}</h5>
-          <h5>{teacher.previous_workshops}</h5>
+          <ul>
+            {teacher.previous_workshops.map((workshop) => (
+              <li key={workshop._id}>{workshop.title}</li>
+            ))}
+          </ul>
         </div>
       )}
     </>

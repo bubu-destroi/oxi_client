e19d4828fd9d08806e3095c7911c994921a4d4d2
bubu-destroi <zzzarolina@gmail.com>
2024-08-30 02:19:09 +0100

    user management styles

--- src/pages/UserManagment.jsx
+++ src/pages/UserManagment.jsx
@@ -1,4 +1,4 @@
-import { useState,  useEffect } from 'react';
+import { useState, useEffect } from 'react';
 //import { AuthContext } from '../context/auth.context';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
@@ -78,161 +78,211 @@ function UserManagement() {
     setApprovedUsers(approvedUsersList);
   }, [allUsers]);
   return (
-    <div className='container mx-auto p-4 pt-20 md:pt-20 md:px-28 lg:px-36'>
+    <div className='container mx-auto p-10 pt-20 md:pt-20 md:px-28 lg:px-36'>
       <h2 className='text-base pt-12 md:text-lg text-center font-semibold mb-4'>
         User Management
       </h2>
 
-      <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-4 mb-7'>
         {/* New Users Section */}
-        <div className='p-2'>
-          <h3 className='text-sm md:text-md text-red-500 font-medium text-center mb-2'>
-            New Users {newUsers.length > 0 && newUsers.length}
+        <div className='relative p-2 h-96 overflow-hidden'>
+          <h3 className='text-sm  md:text-md text-red-500 font-medium text-center mb-2 sticky top-0 z-10 pb-6'>
+            New Users: {newUsers.length > 0 && newUsers.length}
           </h3>
-          {newUsers.length > 0 ? (
-            newUsers.map((user) => (
-              <div
-                className='hover:bg-white p-3 mb-3 shadow-sm max-w-sm mx-auto'
-                key={user._id}>
-                <h5 className='text-xs md:text-xs font-medium text-gray-800 mb-1'>
-                  Learner Username: {user.learner_username}
-                </h5>
-                <h5 className='text-xs md:text-xs text-gray-600 mb-1'>
-                  Age: {user.age}
-                </h5>
-                <h5 className='text-xs md:text-xs text-gray-600 mb-1'>
-                  Parent Name: {user.parent_name}
-                </h5>
-                <div className='mb-2'>
-                  <img
-                    src={user.id_card_picture}
-                    alt={`${user.parent_name}'s ID`}
-                    className='w-full max-w-xs h-auto shadow-sm'
-                  />
+          <div className='overflow-y-auto h-full pt-3 '>
+            {newUsers.length > 0 ? (
+              newUsers.map((user) => (
+                <div
+                  className='hover:bg-white p-3 mb-3 shadow-sm max-w-sm mx-auto '
+                  key={user._id}>
+                  <h5 className='text-xs md:text-xs font-medium text-gray-800 mb-1'>
+                    Learner Username: {user.learner_username}
+                  </h5>
+                  <h5 className='text-xs md:text-xs text-gray-600 mb-1'>
+                    Age: {user.age}
+                  </h5>
+                  <h5 className='text-xs md:text-xs text-gray-600 mb-1'>
+                    Parent Name: {user.parent_name}
+                  </h5>
+                  <div className='flex justify-center mb-2 p-8'>
+                    <img
+                      src={user.id_card_picture}
+                      alt={`${user.parent_name}'s ID`}
+                      className='w-full max-w-xs h-auto shadow-sm'
+                    />
+                  </div>
+                  <div className='flex justify-center'>
+                    <button
+                      className='bg-red-500 text-white py-1 px-3 text-xs md:text-xs hover:bg-blue-600'
+                      type='button'
+                      onClick={() => handleApproveUser(user._id)}>
+                      APPROVE
+                    </button>
+                  </div>
                 </div>
-                <div className='flex justify-center'>
-                  <button
-                    className='bg-red-500 text-white py-1 px-3 text-xs md:text-xs hover:bg-blue-600 '
-                    type='button'
-                    onClick={() => handleApproveUser(user._id)}>
-                    APPROVE
-                  </button>
-                </div>
-              </div>
-            ))
-          ) : (
-            <p className='text-xs md:text-xs text-center'>
-              No new users awaiting approval.
-            </p>
-          )}
+              ))
+            ) : (
+              <p className='text-xs md:text-xs text-center'>
+                No new users awaiting approval.
+              </p>
+            )}
+          </div>
         </div>
 
         {/* Approved Users Section */}
-        <div className='p-2'>
-          <h3 className='text-sm md:text-md text-red-500 font-medium text-center mb-2'>
-            Approved Users {approvedUsers.length > 0 && approvedUsers.length}
+        <div className='relative p-2 h-80 overflow-hidden mb-7'>
+          <h3 className='text-sm  md:text-md text-red-500 font-medium text-center mb-2 sticky top-0 z-10 pb-6'>
+            Approved Users: {approvedUsers.length > 0 && approvedUsers.length}
           </h3>
-          {approvedUsers.length > 0 ? (
-            approvedUsers.map((user) => (
-              <div
-                className='hover:bg-white p-3 mb-3 shadow-sm max-w-sm mx-auto'
-                key={user._id}>
-                <h5 className='text-xs md:text-xs mb-1'>
-                  Learner Username:{' '}
-                  <span className='text-red-500'>{user.learner_username}</span>
-                </h5>
-                <h5 className='text-xs md:text-xs mb-1'>
-                  Age: <span className='text-red-500'>{user.age}</span>
-                </h5>
-                <h5 className='text-xs md:text-xs text-gray-600 mb-1'>
-                  {' '}
-                  Wishes:{' '}
-                  {user.wishes.map((wish, index) => (
-                    <span key={wish._id}>
-                      <Link
-                        to={`/wishes/${wish._id}`}
-                        className='text-blue-500 hover:underline'>
-                        {wish.title}
-                      </Link>
-                      {index < user.wishes.length - 1 && ', '}
+          <div className='overflow-y-auto h-full mb-7'>
+            {approvedUsers.length > 0 ? (
+              approvedUsers.map((user) => (
+                <div
+                  className='hover:bg-white p-3 mb-3 shadow-sm max-w-sm mx-auto'
+                  key={user._id}>
+                  <h5 className='text-xs md:text-xs mb-1'>
+                    Learner Username:{' '}
+                    <span className='text-red-500'>
+                      {user.learner_username}
                     </span>
-                  ))}
-                </h5>
-                <h5 className='text-xs md:text-xs text-gray-600 mb-1'>
-                  Signed Up Workshops:{' '}
-                  {user.signedUp_workshops.map((workshop, index) => (
-                    <span key={workshop._id}>
-                      <Link
-                        to={`/workshops/${workshop._id}`}
-                        className='text-blue-500 hover:underline'>
-                        {workshop.title}
-                      </Link>
-                      {index < user.signedUp_workshops.length - 1 && ', '}
-                    </span>
-                  ))}{' '}
-                </h5>
-                <h5 className='text-xs md:text-xs text-gray-600 mb-1'>
-                  Waiting List:{' '}
-                  {user.userWaitingList.map((workshop, index) => (
-                    <span key={workshop._id}>
-                      <Link
-                        to={`/workshops/${workshop._id}`}
-                        className='text-blue-500 hover:underline'>
-                        {workshop.title}
-                      </Link>
-                      {index < user.userWaitingList.length - 1 && ', '}
-                    </span>
-                  ))}{' '}
-                </h5>
-                <h5 className='text-xs md:text-xs text-gray-600 mb-1'>
-                  Courses Taken:{' '}
-                  {user.courses_taken.map((course, index) => (
-                    <span key={course._id}>
-                      <Link
-                        to={`/courses/${course._id}`}
-                        className='text-blue-500 hover:underline'>
-                        {course.title}
-                      </Link>
-                      {index < user.courses_taken.length - 1 && ', '}
-                    </span>
-                  ))}{' '}
-                </h5>
-              </div>
-            ))
-          ) : (
-            <p className='text-xs md:text-xs text-center'>
-              No approved users found.
-            </p>
-          )}
+                  </h5>
+                  <h5 className='text-xs md:text-xs mb-1'>
+                    Age: <span className='text-red-500'>{user.age}</span>
+                  </h5>
+                  <h5 className='text-xs md:text-xs text-gray-600 mb-1'>
+                    Wishes:{' '}
+                    {user.wishes.map((wish, index) => (
+                      <span key={wish._id} >
+                        <Link
+                          to={`/wishes/${wish._id}`}
+                          className='text-blue-500 hover:underline'>
+                          {wish.title}
+                        </Link>
+                        {index < user.wishes.length - 1 && ', '}
+                      </span>
+                    ))}
+                  </h5>
+                  <h5 className='text-xs md:text-xs text-gray-600 mb-1'>
+                    Signed Up Workshops:{' '}
+                    {user.signedUp_workshops.map((workshop, index) => (
+                      <span key={workshop._id}>
+                        <Link
+                          to={`/workshops/${workshop._id}`}
+                          className='text-blue-500 hover:underline'>
+                          {workshop.title}
+                        </Link>
+                        {index < user.signedUp_workshops.length - 1 && ', '}
+                      </span>
+                    ))}{' '}
+                  </h5>
+                  <h5 className='text-xs md:text-xs text-gray-600 mb-1'>
+                    Waiting List:{' '}
+                    {user.userWaitingList.map((workshop, index) => (
+                      <span key={workshop._id}>
+                        <Link
+                          to={`/workshops/${workshop._id}`}
+                          className='text-blue-500 hover:underline'>
+                          {workshop.title}
+                        </Link>
+                        {index < user.userWaitingList.length - 1 && ', '}
+                      </span>
+                    ))}{' '}
+                  </h5>
+                  <h5 className='text-xs md:text-xs text-gray-600 mb-1'>
+                    Courses Taken:{' '}
+                    {user.courses_taken.map((course, index) => (
+                      <span key={course._id}>
+                        <Link
+                          to={`/courses/${course._id}`}
+                          className='text-blue-500 hover:underline'>
+                          {course.title}
+                        </Link>
+                        {index < user.courses_taken.length - 1 && ', '}
+                      </span>
+                    ))}{' '}
+                  </h5>
+                </div>
+              ))
+            ) : (
+              <p className='text-xs md:text-xs text-center'>
+                No approved users found.
+              </p>
+            )}
+          </div>
         </div>
       </div>
-
+      <div>
       <h3 className='text-sm text-center pt-10 pb-5 md:text-lg font-medium text-red-500 md:text-center mb-2'>
-        The proposals submitted, yet to be approved
+        The proposals submitted, yet to be approved: {proposals.length}
       </h3>
       {proposals.length > 0 && (
-        <div className=' grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-4'>
+        <div className='relative grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 h-80 overflow-y-auto '>
           {proposals.map((proposal) => (
             <div
-              className=' hover:bg-white p-3 shadow-sm  w-full mx-auto '
-              key={proposal._id}>
-              <Link to={`/proposals/${proposal._id}`}>
-                <h5 className='text-xs md:text-xs font-medium text-gray-800 mb-1 hover:text-red-500'>
-                  {proposal.title}
+              className='proposal-detail shadow-white pl-3 pr3 p-6 md:p-6 w-full border border-red-200 hover:bg-white'
+              key={proposal._id}
+            >
+              <h3 className='text-xs  font-bold mb-2 text-center'>Teachers:</h3>
+              <h3 className='text-xs  mb-2 text-center'>{proposal.name}</h3>
+              <h3 className='text-xs  font-bold mb-2 text-center'>Bio:</h3>
+              <h3 className='text-xs  mb-2 text-center'>{proposal.bio}</h3>
+              <h3 className='text-xs  font-bold mb-2 text-center'>Social Media:</h3>
+              <h3 className='text-xs  mb-2 text-center'>{proposal.socialMedia}</h3>
+              <h3 className='text-xs  font-bold mb-2 text-center'>Email:</h3>
+              <h3 className='text-xs  mb-2 text-center'>{proposal.email}</h3>
+              <h3 className='text-xs  font-bold mb-2 text-center text-red-500'>
+                {proposal.title}
+              </h3>
+              <h4 className='text-xs md:text-sm mb-3 text-justify'>
+                {proposal.description}
+              </h4>
+              <div className='w-full flex justify-center py-4'>
+                <img
+                  src={proposal.image}
+                  alt='proposal'
+                  className='w-32 h-32 object-cover'
+                />
+              </div>
+              <div className='text-xs mb-2 mt-4 md:mt-6'>
+                <h5 className='mb-1'>Duration: {proposal.duration}</h5>
+                <h5 className='mb-1'>{proposal.price}€</h5>
+                <h5 className='mb-1'>
+                  {proposal.category}, {proposal.subcategory}
                 </h5>
-              </Link>
-              <div className='flex justify-center'>
+                <h5 className='mb-1'>
+                  Remote: {proposal.remote ? 'Yes' : 'No'}
+                </h5>
+                <h5 className='mb-1'>Location: {proposal.place}</h5>
+                <h5 className='mb-1'>
+                  Date: {proposal.date.split('T')[0]}
+                </h5>
+                <h5 className='mb-1'>
+                  Minimum age required: {proposal.minimum_age}
+                </h5>
+                <h5 className='mb-1'>
+                  Maximum age suggested: {proposal.maximum_age}
+                </h5>
+                <h5 className='mb-1'>
+                  Minimum number of participants: {proposal.minParticipants}
+                </h5>
+                <h5 className='mb-1'>
+                  Maximum number of participants: {proposal.maxParticipants}
+                </h5>
+              </div>
+              <div className='flex justify-center mt-4'>
                 <button
-                  className='bg-red-500 text-white py-1 px-3 text-xs md:text-xs hover:bg-blue-600 '
+                  className='bg-red-500 text-white py-2 px-4 text-xs md:text-sm hover:bg-blue-600'
                   type='button'
-                  onClick={() => handleDeleteProposal(proposal._id)}>
-                  DELETE
+                  onClick={() => handleDeleteProposal(proposal._id)}
+                >
+                  Delete
                 </button>
               </div>
             </div>
           ))}
         </div>
       )}
+    </div>
     </div>
   );
 }
